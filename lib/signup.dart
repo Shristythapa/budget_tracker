f60396e617b0696a47d66a4d17a8e4974b838819
joshiroshan052 <joshiroshan052@gmail.com>
2022-12-07 18:19:26 +0545

    SignUp page updated

--- lib/signup.dart
+++ lib/signup.dart
@@ -29,7 +29,6 @@ class _RegisterScreenState extends State<RegisterScreen> {
       body: SafeArea(
         child: SingleChildScrollView(
           child: Container(
-
             height: MediaQuery.of(context).size.height,
             width: double.infinity,
             child: Column(
@@ -38,141 +37,69 @@ class _RegisterScreenState extends State<RegisterScreen> {
                 Column(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    Text ("Hello there", style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                    ),),
-                    SizedBox(height: 20,),
-                    Text("Create an Account,Its free",style: TextStyle(
-                      fontSize: 15,
-                      color: Colors.grey[700],
-                    ),),
-                    SizedBox(height: 30,)
+                    Text(
+                      "Hello there",
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      "Create an Account,Its free",
+                      style: TextStyle(
+                        fontSize: 15,
+                        color: Colors.grey[700],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    )
                   ],
                 ),
                 Padding(
-                  padding: EdgeInsets.symmetric(
-                      horizontal: 40
-                  ),
+                  padding: EdgeInsets.symmetric(horizontal: 40),
                 ),
                 Container(
-                  padding: EdgeInsets.only(top: 3, left: 3),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(40),
-                    border: Border(
-                        bottom: BorderSide(color: Colors.black),
-                        top: BorderSide(color: Colors.black),
-                        right: BorderSide(color: Colors.black),
-                        left: BorderSide(color: Colors.black)),
-                  ),
+                  padding: EdgeInsets.symmetric(horizontal: 40),
                   child: TextFormField(
                     decoration: InputDecoration(
+                        contentPadding:
+                            EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.grey,
+                          ),
+                        ),
+                        border: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.grey)),
                         label: Text("Full Name"),
                         hintText: "Please enter your full name",
                         prefixIcon: Icon(Icons.accessibility_outlined)),
                   ),
                 ),
+                SizedBox(height: 30.0),
                 Container(
-                  padding: EdgeInsets.only(top: 3, left: 3),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(40),
-                    border: Border(
-                        bottom: BorderSide(color: Colors.black),
-                        top: BorderSide(color: Colors.black),
-                        right: BorderSide(color: Colors.black),
-                        left: BorderSide(color: Colors.black)),
-                  ),
+                  padding: EdgeInsets.symmetric(horizontal: 40),
                   child: TextFormField(
                     decoration: InputDecoration(
+                        contentPadding:
+                            EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.grey,
+                          ),
+                        ),
+                        border: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.grey)),
                         label: Text("Email"),
                         hintText: "Please enter your email",
                         prefixIcon: Icon(Icons.accessibility_outlined)),
                   ),
                 ),
-                Container(
-                  padding: EdgeInsets.only(top: 3, left: 3),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(40),
-                    border: Border(
-                        bottom: BorderSide(color: Colors.black),
-                        top: BorderSide(color: Colors.black),
-                        right: BorderSide(color: Colors.black),
-                        left: BorderSide(color: Colors.black)),
-                  ),
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                        label: Text("Password"),
-                        hintText: "Please enter your password",
-                        prefixIcon: Icon(Icons.visibility)),
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.only(top: 3, left: 3),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(40),
-                    border: Border(
-                        bottom: BorderSide(color: Colors.black),
-                        top: BorderSide(color: Colors.black),
-                        right: BorderSide(color: Colors.black),
-                        left: BorderSide(color: Colors.black)),
-                  ),
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                        label: Text("Confirm Password"),
-                        hintText: "Please re-enter your password",
-                        prefixIcon: Icon(Icons.visibility)),
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.only(top: 3, left: 3),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(40),
-                    border: Border(
-                        bottom: BorderSide(color: Colors.black),
-                        top: BorderSide(color: Colors.black),
-                        right: BorderSide(color: Colors.black),
-                        left: BorderSide(color: Colors.black)),
-                  ),
-                  child: MaterialButton(
-                    minWidth: double.infinity,
-                    height: 60,
-                    onPressed: () {},
-                    color: Colors.redAccent,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(40)),
-                    child: Text(
-                      "Sign Up",
-                      style: TextStyle(
-                        fontWeight: FontWeight.w600,
-                        fontSize: 16,
-                      ),
-                    ),
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                  child: Row(
-                    children: [
-                      Text("Already have an account?"),
-                      SizedBox(
-                        width: 5,
-                      ),
-                      InkWell(
-                        //   onTap: () {
-                        //     Navigator.of(context).pushReplacement(MaterialPageRoute(
-                        //       builder: (BuildContext context) => LoginScreen(),
-                        //     )
-                        //     );
-                        //   },
-                        child: Text(
-                          "Go to login",
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold, color: Colors.blue),
-                        ),
-                      )
-                    ],
-                  ),
-                )
+
               ],
             ),
           ),
