706044d9f8cbf30eb6e124aaca348541189b9062
Shristythapa <thapashristy110@gmail.com>
2022-12-09 11:55:06 +0545

    landing page frontend added

---
+++ lib/landingpage.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+class LandingPage extends StatelessWidget {
+  const LandingPage({super.key});
+
+  
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return MaterialApp(
+      theme: ThemeData(
+          scaffoldBackgroundColor: Color.fromARGB(255, 161, 207, 245)),
+      debugShowCheckedModeBanner: false,
+      home: MyLandingPage(),
+    );
+  }
+}
+
+class MyLandingPage extends StatefulWidget {
+  const MyLandingPage({super.key});
+
+  @override
+  State<MyLandingPage> createState() => _MyLandingPageState();
+}
+
+class _MyLandingPageState extends State<MyLandingPage> {
+  @override
+  Widget build(BuildContext context) {
+    var deviceHeight = MediaQuery.of(context).size.height;
+    return Scaffold(
+        body: Container(
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+            image: AssetImage("images/landing.jpg"), fit: BoxFit.cover),
+      ),
+      child: Column(
+        children: [
+          
+         Padding(padding: EdgeInsets.fromLTRB(20, deviceHeight/100*55, 20, 20),
+         child:  Center(
+              child: Container(
+            decoration: BoxDecoration(
+              
+            ),
+            width: 230,
+            height: 70,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color(0xFF296157),
+                foregroundColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20)
+                )
+              ),
+              onPressed: () {
+                // Navigator.of(context).pop();
+                // Navigator.push(
+                //   context,
+                //   MaterialPageRoute(builder: (context) => const Signup()),
+                // );
+              },
+              child: Text(
+                "Join Now",
+                style: TextStyle(fontSize: 30),
+              ),
+            ),
+          )),),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+            child: Center(
+                child: InkWell(
+              child: Text(
+                "Already have an account?",
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Color.fromARGB(255, 6, 69, 16),
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              onTap: () {
+                // Navigator.of(context).pop();
+                // Navigator.push(
+                //   context,
+                //   MaterialPageRoute(builder: (context) => const ),
+                // );
+              },
+            )),
+          )
+        ],
+      ),
+    ));
+  }
+}
