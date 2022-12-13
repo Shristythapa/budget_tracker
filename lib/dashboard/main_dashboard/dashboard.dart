3e159da414b95a39d61a64fc503960f673d2c38f
sabinalopchan <sabinalopchan321@gmail.com>
2022-12-13 21:48:40 +0545

    dashboard screen

---
+++ lib/dashboard/main_dashboard/dashboard.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+import '../../common/custom_box.dart';
+import 'dashboard_body.dart';
+import 'dashboard_header.dart';
+
+class DashboardScreen extends StatefulWidget {
+  const DashboardScreen({super.key});
+
+  @override
+  State<DashboardScreen> createState() => _DashboardScreenState();
+}
+
+class _DashboardScreenState extends State<DashboardScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       body:SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.symmetric(horizontal:14.0),
+              child: Column(
+                children: [
+                  DashboardHeader(),
+                  SizedBox(height:30),
+                  // DashboardBody(),
+                ],
+              ),
+            ),
+            DashboardBody(),
+          ],
+        ),
+        )
+    );
+  }
+}
