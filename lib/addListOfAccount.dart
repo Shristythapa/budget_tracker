65952f5d2a9fd28627b453acc6c2bdbe8b95ce41
joshiroshan052 <joshiroshan052@gmail.com>
2022-12-13 21:49:06 +0545

    AddAccount Created

---
+++ lib/addListOfAccount.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AddListOfAccount extends StatefulWidget {
+  const AddListOfAccount({Key? key}) : super(key: key);
+
+  @override
+  State<AddListOfAccount> createState() => _addListOfAccountState();
+}
+
+class _addListOfAccountState extends State<AddListOfAccount> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
