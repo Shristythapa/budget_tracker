d51b94ef8325e7ce5598c32b300557c04ae746e5
joshiroshan052 <joshiroshan052@gmail.com>
2022-12-14 18:56:37 +0545

    List Of Account Updated

--- lib/addListOfAccount.dart
+++ lib/addListOfAccount.dart
@@ -12,7 +12,29 @@ class _addListOfAccountState extends State<AddListOfAccount> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(
+              bottom: Radius.circular(20),
+            )),
+        backgroundColor: Color(0xFF296157),
+        actions: <Widget>[
+          IconButton(
+              icon: const Icon(Icons.cancel_outlined,size: 32,),
+              tooltip: 'Cancel',
+              color: Colors.black87,
+              onPressed: ()=> showDialog<String>(
+                context:context,
+                builder:(BuildContext context) => AlertDialog(
+                  title: const Text('Do you sure want to quit?'),
+                  actions: <Widget>[
+                    TextButton(
+                      onPressed:(
+                          ) {},
+                      child: const Text('No'),
 
+        ],
+      ),
     );
   }
 }
