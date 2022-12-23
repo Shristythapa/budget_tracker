fb45f0b147365e742c5f8edf862c0aa09bdf00a8
unknown <sabinalopchan321@gmail.com>
2022-12-23 07:18:33 +0545

    balance transfer widget

---
+++ lib/balanceTransfer/balance_transfer_widget.dart
@@ -0,0 +1,84 @@
+import 'package:budget_tracer_practice/common/custom_box.dart';
+import 'package:budget_tracer_practice/common/custom_button.dart';
+import 'package:budget_tracer_practice/common/custom_dropdown.dart';
+import 'package:budget_tracer_practice/common/custom_icon.dart';
+import 'package:budget_tracer_practice/common/custom_textfield.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class BalanceTransferWidget extends StatelessWidget {
+   BalanceTransferWidget({super.key});
+
+  final _formKey = GlobalKey<FormState>();
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      body:ListView(
+        children: 
+          [Form(
+            key: _formKey,
+            child: Container(
+              
+              margin:EdgeInsets.only(top:MediaQuery.of(context).viewPadding.top),
+              padding: EdgeInsets.symmetric(horizontal: 14.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CustomIcon(
+                    icon: Icons.arrow_back_outlined,
+                    ),
+                    SizedBox(height: 20,),
+                    Text(
+                      "Transfer Balance",
+                      style:TextStyle(
+                       fontSize:32,
+                       fontWeight: FontWeight.w400,
+                     )
+                      ),
+                      SizedBox(height: 5,),
+                      CustomTextField(
+                        hintText: "Amount",
+                        validator:(val){
+                            final  amountValid =RegExp(
+                              r"^([0-9])");
+                                if (val ==null){
+                                  return"Email field cannot be empty";
+                                }else if (val.isEmpty){
+                                  return"Email field cannot be empty";
+                                }else if (amountValid.hasMatch(val)){
+                                  return null;
+                                }else{
+                                  return "Enter amount in numbers";
+                                }
+                            },
+                        ),
+                        SizedBox(height: 5,),
+          
+                        CustomDropDown(hint: "Select account to transfer from",),
+                        CustomDropDown(hint: "Select account to transfer to"),
+                        SizedBox(height: 60,),
+                        CustomButton(
+                          title: "Add",
+                          onPressed: () {
+                              if (_formKey.currentState!.validate()) {
+                                _formKey.currentState!.save();
+                              }
+                            },
+                          ),
+                          SizedBox(height: 40,),
+                          CustomButton(
+                          title: "Cancel",
+                          ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      )
+    );
+  }
+}
