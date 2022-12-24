dbb4c0ade31389ae712b16d9cdd47b9926c48f17
unknown <sabinalopchan321@gmail.com>
2022-12-25 00:20:16 +0545

    custom widgets

---
+++ lib/common/custom_box.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class CustomBox extends StatefulWidget {
+  final String title;
+  final String rs;
+  const CustomBox({
+    super.key,
+    required this.title,
+    required this.rs,
+  });
+ 
+
+  @override
+  State<CustomBox> createState() => _CustomBoxState();
+}
+
+class _CustomBoxState extends State<CustomBox> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(12)),
+        color: Colors.grey[300],
+      ),
+      padding: EdgeInsets.symmetric(horizontal: 30,vertical:20),
+      child: Row(
+        children: [
+          Expanded(
+            child: Text(
+              widget.title,
+               style:TextStyle(
+                fontSize:16,
+                fontWeight:FontWeight.w400,
+              )
+            ),
+          ),
+          Text(
+            widget.rs,
+             style:TextStyle(
+                fontSize:16,
+                fontWeight:FontWeight.w400,
+              )
+            ),
+           
+        ],
+      ),
+    );
+  }
+}
--- lib/common/custom_dropdown.dart
+++ lib/common/custom_dropdown.dart
@@ -1,3 +1,4 @@
+import 'package:control_style/decorated_input_border.dart';
 import 'package:dropdown_button2/dropdown_button2.dart';
 import 'package:flutter/material.dart';
 
@@ -26,66 +27,73 @@ final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Container(
+      
        child: Column(
         children: [
            SizedBox(height: 30),
-          Material(
-            borderRadius: BorderRadius.all(Radius.circular(12)),
-            child: DropdownButtonFormField2(
-              decoration: InputDecoration(
-                isDense: true,
-                contentPadding: EdgeInsets.zero,
-                  border: OutlineInputBorder(
-                         borderRadius: BorderRadius.all(Radius.circular(30.0)),
-                          borderSide: BorderSide(
-                          width: 0, 
-                          style: BorderStyle.none,
-                      ),
-                        ),
-                          filled:true,
-                  fillColor:Colors.white,
-              ),
-              isExpanded: true,
-              hint:  Text(
-                widget.hint,
-                style: TextStyle(fontSize: 16),
-              ),
-              icon: const Icon(
-                Icons.arrow_drop_down,
-                color: Colors.black45,
-              ),
-              iconSize: 30,
-              buttonHeight: 50,
-              buttonPadding: const EdgeInsets.only(left: 20, right: 10),
-              dropdownDecoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15),
-              ),
-              items: bankItems
-                      .map((item) =>
-                      DropdownMenuItem<String>(
-                        value: item,
-                        child: Text(
-                          item,
-                          style: const TextStyle(
-                            fontSize: 16,
+           
+          DropdownButtonFormField2(
+            decoration: InputDecoration(
+              border: DecoratedInputBorder(
+              child: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8),
+                borderSide: BorderSide(
+                              width: 0, 
+                              style: BorderStyle.none,
                           ),
-                        ),
-                      ))
-                      .toList(),
-              validator: (value) {
-                if (value == null) {
-                  return 'Please select bank account.';
-                }
-              },
-              onChanged: (value) {
-                //Do something when changing the item if you want.
-              },
-              onSaved: (value) {
-                selectedValue = value.toString();
-              },
+              ),
+              shadow:[
+                BoxShadow(
+                  color: Color.fromARGB(255, 174, 175, 175),
+                  blurRadius: 12,
+                )
+              ],
             ),
+            contentPadding: EdgeInsets.zero,
+            filled: true,
+            fillColor: Colors.white,
+      ),
+      
+              isDense: true,
+            hint:  Text(
+              widget.hint,
+              style: TextStyle(fontSize: 16),
+            ),
+            icon: const Icon(
+              Icons.arrow_drop_down,
+              color: Colors.black45,
+            ),
+            iconSize: 30,
+            buttonHeight: 50,
+            // buttonPadding: const EdgeInsets.only(left: 20, right: 10),
+            dropdownDecoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+            ),
+            items: bankItems
+                    .map((item) =>
+                    DropdownMenuItem<String>(
+                      value: item,
+                      child: Text(
+                        item,
+                        style: const TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                    ))
+                    .toList(),
+            validator: (value) {
+              if (value == null) {
+                return 'Please select bank account.';
+              }
+            },
+            onChanged: (value) {
+              //Do something when changing the item if you want.
+            },
+            onSaved: (value) {
+              selectedValue = value.toString();
+            },
           ),
-          const SizedBox(height: 30),
+          // const SizedBox(height: 30),
          
         ],
       ),
