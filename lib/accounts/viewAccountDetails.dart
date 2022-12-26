b7f0e5279fc7a8879583c55177dc7d15f8178d24
joshiroshan052 <joshiroshan052@gmail.com>
2022-12-26 11:32:04 +0545

    Delete button Completed

--- lib/accounts/viewAccountDetails.dart
+++ lib/accounts/viewAccountDetails.dart
@@ -84,7 +84,14 @@ class MyAccountDetails extends StatelessWidget {
                       context: context,
                       builder: (BuildContext context) => AlertDialog(
                         title: const Text('Do you sure want to delete?'),
-                        
+                        actions: <Widget>[
+                          TextButton(
+                            onPressed: () {},
+                            child: const Text('No'),
+                          ),
+                          TextButton(
+                            onPressed: () {},
+                            child: const Text('Yes'),
                           ),
                         ],
                       ),
