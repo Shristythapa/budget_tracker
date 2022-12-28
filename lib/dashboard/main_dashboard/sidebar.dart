92c5d668e9c88575536612b93cb46a1e6efacc55
dhawit <tdhawit@gmail.com>
2022-12-28 11:16:24 +0545

    final

--- lib/dashboard/main_dashboard/sidebar.dart
+++ lib/dashboard/main_dashboard/sidebar.dart
@@ -30,3 +30,170 @@ class _sidebarState extends State<sidebar> {
                   size: 50,
                 )),
           ),
+          Stack(
+            clipBehavior: Clip.none,
+            children: [
+              Container(
+                height: 572,
+                decoration: BoxDecoration(
+                    color: Color(0xFF77ABA2),
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(70),
+                      topRight: Radius.circular(70),
+                    )),
+                // width: 150,
+              ),
+              Positioned(
+                child: GestureDetector(
+                  onTap: () {
+                    //     Navigator.push(
+                    // context,
+                    // MaterialPageRoute(builder: (context)=>DashboardScreen()));
+                    // print("Container");
+                  },
+                  child: Column(
+                    children: [
+                      Container(
+                        height: 85,
+                        width: 315,
+                        decoration: BoxDecoration(
+                          color: Color(0xFFEFEFEF),
+                          borderRadius: BorderRadius.circular(20),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Color(0xFFA10564A).withOpacity(0.5),
+                              spreadRadius: 5,
+                              blurRadius: 10,
+                              offset:
+                                  Offset(0, 5), // changes position of shadow
+                            ),
+                          ],
+                        ),
+                        child: Row(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(top: 1, left: 20),
+                              child: ImageIcon(
+                                AssetImage("assets/images/wallet.png"),
+                                size: 60,
+                                color: Colors.black,
+                              ),
+                            ),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.only(top: 10, left: 100),
+                              child: Text("Wallet",
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w400,
+                                    fontSize: 25,
+                                  )),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                right: 0,
+                left: 0,
+                top: 160,
+              ),
+
+              // second
+              Positioned(
+                child: Column(
+                  children: [
+                    Container(
+                      height: 85,
+                      width: 315,
+                      decoration: BoxDecoration(
+                        color: Color(0xFFEFEFEF),
+                        borderRadius: BorderRadius.circular(20),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Color(0xFFA10564A).withOpacity(0.5),
+                            spreadRadius: 5,
+                            blurRadius: 10,
+                            offset: Offset(0, 5), // changes position of shadow
+                          ),
+                        ],
+                      ),
+                      child: Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(top: 1, left: 20),
+                            child: ImageIcon(
+                              AssetImage("assets/images/pie-chart.png"),
+                              size: 60,
+                              color: Colors.black,
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 10, left: 100),
+                            child: Text("Chart",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w400,
+                                  fontSize: 25,
+                                )),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                right: 0,
+                left: 0,
+                top: 280,
+              ),
+              Positioned(
+                child: Column(
+                  children: [
+                    Container(
+                      height: 85,
+                      width: 315,
+                      decoration: BoxDecoration(
+                        color: Color(0xFFEFEFEF),
+                        borderRadius: BorderRadius.circular(20),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Color(0xFFA10564A).withOpacity(0.5),
+                            spreadRadius: 5,
+                            blurRadius: 10,
+                            offset: Offset(0, 5), // changes position of shadow
+                          ),
+                        ],
+                      ),
+                      child: Row(
+                        children: [
+                          const Padding(
+                            padding: EdgeInsets.only(top: 1, left: 20),
+                            child: ImageIcon(
+                              AssetImage("assets/images/money-saving.png"),
+                              size: 60,
+                              color: Colors.black,
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 10, left: 100),
+                            child: Text("Savings",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w400,
+                                  fontSize: 25,
+                                )),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                right: 0,
+                left: 0,
+                top: 400,
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
