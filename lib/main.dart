b7d0e6cc301fcb7e5e4a8ac7002bf2d453b7e450
dhawit <tdhawit@gmail.com>
2023-01-04 18:07:22 +0545

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
+import 'package:budget_tracer_practice/expenses/food.dart';
 import 'package:budget_tracer_practice/login.dart';
 import 'package:flutter/material.dart';
 import 'dart:io';
-
 
 void main() {
   runApp(const MyApp());
@@ -27,7 +27,6 @@ class MyApp extends StatelessWidget {
           // is not restarted.
           primarySwatch: Colors.blue,
         ),
-        home: loginScreen());
+        home: food());
   }
 }
-
