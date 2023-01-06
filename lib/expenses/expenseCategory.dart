c77156679f87c9cb05bdc76b2e230718650adb69
joshiroshan052 <joshiroshan052@gmail.com>
2023-01-06 11:41:55 +0545

    Expense Category Edited

--- lib/expenses/expenseCategory.dart
+++ lib/expenses/expenseCategory.dart
@@ -7,4 +7,21 @@ class ExpenseCatgery extends StatelessWidget {
     "name": "${['School Fees', 'Fuel', 'Goseries', 'Clothings','Medicines', 'Gift', 'Stationary', 'House Rent','Travel'][index]}"}).toList();
 
 
- 
+  @override
+  Widget build(BuildContext context) {
+    print(MediaQuery.of(context).size.width);
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(
+            Icons.close,
+            color: Colors.black87,
+          ),
+          onPressed: () {
+            // do something
+          },
+        ),
+       
+    );
+  }
+}
