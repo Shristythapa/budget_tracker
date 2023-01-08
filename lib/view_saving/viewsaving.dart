e7a8af3d679bd3eeb88f1f37993d6931782acddf
sumansahs <sumansah900@gmail.com>
2023-01-08 11:54:49 +0545

    :hammer: files on the view savings

---
+++ lib/view_saving/viewsaving.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class SavingsDetails extends StatefulWidget {
+  const SavingsDetails({super.key});
+
+  @override
+  State<SavingsDetails> createState() => _SavingsDetailsState();
+}
