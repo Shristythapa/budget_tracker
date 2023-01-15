7318724f6506acbebb051a53539e99cafa4e0269
joshiroshan052 <joshiroshan052@gmail.com>
2023-01-15 20:07:24 +0545

    Bar Chart Color Added

--- lib/bar_chart/barchart.dart
+++ lib/bar_chart/barchart.dart
@@ -22,7 +22,26 @@ class BarChart extends StatelessWidget {
       financial: 100,
       color: charts.ColorUtil.fromDartColor(Color(0xFFB6ADCA)),
     ),
-   
+    BarChartModel(
+      date: "2022-Jan",
+      financial: 100,
+      color: charts.ColorUtil.fromDartColor(Color(0xFFB6ADCA)),
+    ),
+
+
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    List<charts.Series<BarChartModel, String>> series =[
+      charts.Series(
+        id:"financial",
+        data: data,
+        domainFn: (BarChartModel series, _)=>series.date,
+        measureFn: (BarChartModel series, _)=> series.financial,
+        colorFn: (BarChartModel series, _)=> series.color,
+      ),
+    
       ),
     );
   }
