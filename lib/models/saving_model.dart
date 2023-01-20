acad0256e88fbbb8a2effc7a98d2e639e87dcd64
unknown <sabinalopchan321@gmail.com>
2023-01-21 00:07:36 +0545

    add saving model

---
+++ lib/models/saving_model.dart
@@ -0,0 +1,46 @@
+
+
+import 'dart:convert';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+SavingModel savingModelFromJson(String str) =>
+    SavingModel.fromJson(json.decode(str));
+
+String savingModelToJson(SavingModel data) => json.encode(data.toJson());
+
+class SavingModel {
+  SavingModel({
+    this.id,
+    // this.userId,
+    this.accountname,
+    this.amount,
+  });
+
+  String? id;
+  // String? userId;
+  String? accountname;
+  String? amount;
+
+  factory SavingModel.fromJson(Map<String, dynamic> json) => SavingModel(
+        id: json["id"],
+        // userId: json["user_id"],
+        accountname: json["accountname"],
+        amount: json["amount"],
+      );
+  factory SavingModel.fromFireBaseSnapshot(
+          DocumentSnapshot<Map<String, dynamic>> json) =>
+      SavingModel(
+        id: json.id,
+        // userId: json["user_id"],
+        accountname: json["accountname"],
+        amount: json["amount"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        // "user_id": userId,
+        "accountname": accountname,
+        "amount": amount,
+      };
+}
