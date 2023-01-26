100259fee47855527345916a7e008df8ec565d86
unknown <sabinalopchan321@gmail.com>
2023-01-26 17:31:34 +0545

    add income updated

--- lib/income/addincome.dart
+++ lib/income/addincome.dart
@@ -1,4 +1,5 @@
 import 'package:budget_tracer_practice/common/custom_dropdown.dart';
+import 'package:control_style/decorated_input_border.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -21,7 +22,7 @@ class _IncomeState extends State<Income> {
 
   String dropdownvalue = 'Prime Bank';
   String selectionvalue = 'Category';
- 
+
   TextEditingController email = new TextEditingController();
   TextEditingController password = new TextEditingController();
   TextEditingController date = new TextEditingController();
@@ -59,8 +60,41 @@ class _IncomeState extends State<Income> {
                 CustomTextField(
                   hintText: "Amount",
                 ),
-                CustomDropDown(
-                  hint: "Date",
+                SizedBox(height: 40,),
+                Container(
+                  child: DateTimePicker(
+                    initialValue: '',
+                    firstDate: DateTime(2000),
+                    lastDate: DateTime(2100),
+                    dateLabelText: 'Date',
+                    onChanged: (val) => print(val),
+                    validator: (val) {
+                      print(val);
+                      return null;
+                    },
+                    onSaved: (val) => print(val),
+                    decoration: InputDecoration(
+                        border: DecoratedInputBorder(
+                          child: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8),
+                            borderSide: BorderSide(
+                              width: 0,
+                              style: BorderStyle.none,
+                            ),
+                          ),
+                          shadow: [
+                            BoxShadow(
+                              color: Color.fromARGB(255, 174, 175, 175),
+                              blurRadius: 12,
+                            )
+                          ],
+                        ),
+                        contentPadding: EdgeInsets.zero,
+                        filled: true,
+                        fillColor: Colors.white,
+                        hintText: "Date",
+                        prefixIcon: Icon(Icons.date_range)),
+                  ),
                 ),
                 SizedBox(
                   height: 10,
@@ -98,5 +132,218 @@ class _IncomeState extends State<Income> {
         ),
       ],
     ));
+    // return Scaffold(
+    //   backgroundColor: Color.fromARGB(255, 252, 252, 252),
+    //   body: Form(
+    //     key: form,
+    //     child: Column(
+    //       mainAxisAlignment: MainAxisAlignment.center,
+    //       children: [
+
+    //         const SizedBox(
+    //           height: 20,
+    //         ),
+    //         Padding(
+    //             padding: EdgeInsets.symmetric(horizontal: 20.0),
+    //             child: Column(
+    //                 crossAxisAlignment: CrossAxisAlignment.start,
+    //                 children: <Widget>[
+    //                   Text("Add Income",
+    //                       style: TextStyle(
+    //                         fontSize: 30,
+    //                         color: Color.fromARGB(255, 0, 0, 0),
+    //                         fontWeight: FontWeight.bold,
+    //                       ))
+    //                 ])),
+    //         const SizedBox(
+    //           height: 20,
+    //         ),
+    //         Container(
+    //           margin: EdgeInsets.all(10),
+    //           child: TextFormField(
+    //             controller: email,
+    //             validator: (String? value) {
+    //               if (value == null || value == "") {
+    //                 return "Title is required";
+    //               }
+
+    //               return null;
+    //             },
+    //             decoration: InputDecoration(
+    //                 border: OutlineInputBorder(
+    //                     borderRadius: BorderRadius.circular(8)),
+    //                 hintText: "Title",
+    //                 prefixIcon: Icon(Icons.description)),
+    //           ),
+    //         ),
+    //         const SizedBox(
+    //           height: 20,
+    //         ),
+
+    //         Container(
+    //           margin: EdgeInsets.all(10),
+    //           child: DateTimePicker(
+    //             initialValue: '',
+    //             firstDate: DateTime(2000),
+    //             lastDate: DateTime(2100),
+    //             dateLabelText: 'Date',
+    //             onChanged: (val) => print(val),
+    //             validator: (val) {
+    //               print(val);
+    //               return null;
+    //             },
+    //             onSaved: (val) => print(val),
+    //             // locale: Locale("en", "CN"),
+    //             decoration: InputDecoration(
+    //                 border: OutlineInputBorder(
+    //                     borderRadius: BorderRadius.circular(8)),
+    //                 hintText: "Date",
+    //                 prefixIcon: Icon(Icons.date_range)),
+    //           ),
+
+    //         ),
+
+    //         const SizedBox(
+    //           height: 10,
+    //         ),
+    //         Container(
+    //           margin: EdgeInsets.all(10),
+    //           child: TextFormField(
+    //             controller: password,
+    //             validator: (String? value) {
+    //               if (value == null || value == "") {
+    //                 return "Amount is required";
+    //               }
+    //               return null;
+    //             },
+
+    //             decoration: InputDecoration(
+    //                 border: OutlineInputBorder(
+    //                     borderRadius: BorderRadius.circular(8)),
+    //                 hintText: "Amount",
+    //                 prefixIcon: Icon(Icons.monetization_on),
+    //                     ),
+    //           ),
+    //         ),
+    //         SizedBox(
+    //           height: 10,
+    //           width: 1000,
+    //         ),
+    //         Container(
+    //         margin: EdgeInsets.symmetric(horizontal: 60),
+
+    //          width: double.infinity,
+    //           decoration:BoxDecoration(
+    //              color: Colors.grey,borderRadius: BorderRadius.circular(20)),
+    //           child: DropdownButton(
+    //             value: dropdownvalue,
+    //             icon: const Icon(Icons.keyboard_arrow_down),
+    //             items: items.map((String items) {
+    //               return DropdownMenuItem(
+    //                 value: items,
+    //                 child: Text(items),
+    //               );
+    //             }).toList(),
+    //             onChanged: (String? newValue) {
+    //               setState(() {
+    //                 dropdownvalue = newValue!;
+    //               });
+    //             },
+    //             hint: Text("Account"),
+    //           ),
+
+    //         ),
+    //         SizedBox(
+    //           height: 10,
+    //           width: 1000,
+    //         ),
+    //         Container(
+    //           width: double.infinity,
+    //           margin: EdgeInsets.symmetric(horizontal: 60),
+
+    //           decoration:BoxDecoration(
+    //             color: Colors.grey,
+    //             borderRadius: BorderRadius.circular(20)),
+
+    //           child: DropdownButton(
+    //             value: selectionvalue,
+    //             icon: const Icon(Icons.keyboard_arrow_down),
+    //             items: categories.map((String items) {
+    //               return DropdownMenuItem(
+    //                 value: items,
+    //                 child: Text(items),
+    //               );
+    //             }).toList(),
+    //             onChanged: (String? newValue) {
+    //               setState(() {
+    //                 selectionvalue = newValue!;
+    //               });
+    //             },
+    //             hint: Text("Account"),
+    //           ),
+    //         ),
+    //         SizedBox(
+    //           height: 20.0,
+    //         ),
+
+    //         Container(
+    //           height: 50,
+    //           width: 300,
+    //           margin: EdgeInsets.symmetric(horizontal: 60),
+    //           decoration: BoxDecoration(
+    //             borderRadius: BorderRadius.circular(50),
+    //             color: Color.fromRGBO(49, 39, 79, 1),
+    //           ),
+    //           child: ElevatedButton(
+    //             onPressed: (() {
+    //               if (form.currentState!.validate()) {
+    //                 Add();
+    //               } else {
+    //                 print("Fail");
+    //               }
+    //             }),
+    //             child: Text(
+    //               "Add",
+    //               style: TextStyle(color: Colors.white),
+    //             ),
+    //             style: ElevatedButton.styleFrom(
+    //               shape: RoundedRectangleBorder(
+    //                 borderRadius: BorderRadius.circular(30.0),
+    //               ),
+    //               backgroundColor: Color(0xFF296157),
+    //               foregroundColor: Colors.white,
+    //             ),
+    //           ),
+    //         ),
+    //         SizedBox(
+    //           height: 20.0,
+    //         ),
+    //         Container(
+    //           height: 50,
+    //           width: 300,
+    //           margin: EdgeInsets.symmetric(horizontal: 60),
+    //           decoration: BoxDecoration(
+    //             borderRadius: BorderRadius.circular(50),
+    //             color: Color.fromRGBO(49, 39, 79, 1),
+    //           ),
+    //           child: ElevatedButton(
+    //             onPressed: (() {}),
+    //             child: Text(
+    //               "Cancel",
+    //               style: TextStyle(color: Colors.white),
+    //             ),
+    //             style: ElevatedButton.styleFrom(
+    //               shape: RoundedRectangleBorder(
+    //                 borderRadius: BorderRadius.circular(30.0),
+    //               ),
+    //               backgroundColor: Color(0xFF296157),
+    //               foregroundColor: Colors.white,
+    //             ),
+    //           ),
+    //         ),
+    //       ],
+    //     ),
+    //   ),
+    // );
   }
 }
