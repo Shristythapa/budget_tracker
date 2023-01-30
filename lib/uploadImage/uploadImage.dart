9ec7ca887005c9ce900843d3388600bb4f1b7a12
sumansahs <sumansah900@gmail.com>
2023-01-30 11:17:43 +0545

    upload image pushed

--- lib/uploadImage/uploadImage.dart
+++ lib/uploadImage/uploadImage.dart
@@ -1,129 +1,192 @@
+import 'dart:io';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:image_picker/image_picker.dart';
 
-class PictureUpload extends StatefulWidget {
-  const PictureUpload({super.key});
+class Project extends StatefulWidget {
+  Project({Key? key}) : super(key: key);
 
   @override
-  State<PictureUpload> createState() => _PictureUploadState();
+  _ProjectState createState() => _ProjectState();
 }
 
-class _PictureUploadState extends State<PictureUpload> {
+class _ProjectState extends State<Project> {
+
+
+  File? _image;
+  Future<void> getImage(ImageSource source) async {
+    final image = await ImagePicker().pickImage(source: source);
+    if (image == null) {
+      return;
+    }
+    final imageTemporary = File(image.path);
+
+    setState(() {
+      this._image = imageTemporary;
+    });
+  }
+
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _userController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          elevation: 0,
-          backgroundColor: Colors.transparent,
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Container(
-                width: 150,
-                height: 150,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: Color(0xFF296157),
+      return Scaffold(
+          body: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        CircleAvatar(
+                          radius: 100,
+                          backgroundColor: Colors.white,
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(100),
+                            child: _image != null
+                                ? Image.file(
+                              _image!,
+                              fit: BoxFit.cover,
+                            )
+                                : Image.network(
+                              "https://imgs.search.brave.com/VfOlmssamn3NTAP14DFpqr1z9pxdR7P4czo10TKxRuk/rs:fit:860:681:1/g:ce/aHR0cHM6Ly93d3cu/cG5naXRlbS5jb20v/cGltZ3MvbS8xNDYt/MTQ2ODQ3OV9teS1w/cm9maWxlLWljb24t/YmxhbmstcHJvZmls/ZS1waWN0dXJlLWNp/cmNsZS1oZC5wbmc",
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          top: 125,
+                          left: 100,
+                          width: 90,
+                          height: 30,
+                          child: ActionChip(
+                            label: Text("Change"),
+                            onPressed: () => getImage(ImageSource.gallery),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
-                child: Container(
-                  margin: EdgeInsets.all(6),
-                  child: CircleAvatar(
-                    radius: 20,
-                    backgroundImage:
-                        AssetImage("images/dummyProfileImage.jfif"),
-                  ),
+                SizedBox(
+                  height: 20,
                 ),
-              ),
-               Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
-                child: Container(
-                  padding: EdgeInsets.all(20),
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                  height: 200,
                   decoration: BoxDecoration(
-                      color: Color.fromARGB(255, 219, 220, 220),
-                      borderRadius: BorderRadius.all(Radius.circular(20))),
-                  child: Row(
+                    color: Colors.white54,
+                    borderRadius: BorderRadius.circular(40),
+                  ),
+                  padding: EdgeInsets.all(30),
+                  child: Column(
                     children: [
-                      Icon(
-                        Icons.person,
-                        color: Color.fromARGB(255, 158, 158, 158),
-                        size: 24.0,
+                      TextFormField(
+                        controller: _userController,
+                        autofocus: true,
+                        cursorColor: Colors.green,
+                        cursorHeight: 20,
+                        keyboardType: TextInputType.emailAddress,
+                        // validator: ValidateLogin.emailValidate,
+                        style: const TextStyle(fontSize: 16.0, color: Colors.green),
+                        decoration: InputDecoration(
+                            filled: true,
+                            fillColor: Colors.grey.shade200,
+                            enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(20),
+                                borderSide: BorderSide.none),
+                            focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(20),
+                                borderSide: BorderSide.none),
+                            prefixIcon: const Icon(
+                              Icons.person,
+                              color: Colors.green,
+                              size: 20,
+                            ),
+                            hintText: "Username",
+                            hintStyle: TextStyle(fontSize: 17.0)),
                       ),
-                      SizedBox(width: 20),
-                      Expanded(
-                          child: Text(
-                        "Rin Okumura",
-                      ))
+                      SizedBox(
+                        height: 15,
+                      ),
+                      TextFormField(
+                        controller: _emailController,
+                        autofocus: true,
+                        cursorColor: Colors.green,
+                        cursorHeight: 20,
+                        keyboardType: TextInputType.emailAddress,
+                        // validator: ValidateLogin.emailValidate,
+                        style: const TextStyle(fontSize: 16.0, color: Colors.green),
+                        decoration: InputDecoration(
+                            filled: true,
+                            fillColor: Colors.grey.shade200,
+                            enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(20),
+                                borderSide: BorderSide.none),
+                            focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(20),
+                                borderSide: BorderSide.none),
+                            prefixIcon: const Icon(
+                              Icons.email_rounded,
+                              color: Colors.green,
+                              size: 20,
+                            ),
+                            hintText: "Email Address",
+                            hintStyle: TextStyle(fontSize: 17.0)),
+                      ),
                     ],
                   ),
                 ),
-              ),
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
-                child: Container(
-                  padding: EdgeInsets.all(20),
-                  decoration: BoxDecoration(
-                      color: Color.fromARGB(255, 219, 220, 220),
-                      borderRadius: BorderRadius.all(Radius.circular(20))),
-                  child: Row(
-                    children: [
-                      Icon(
-                        Icons.mail,
-                        color: Color.fromARGB(255, 158, 158, 158),
-                        size: 24.0,
-                      ),
-                      SizedBox(width: 20),
-                      Expanded(
-                          child: Text(
-                        "rin@gmail.com",
-                      ))
-                    ],
+                Container(
+                  width: 300,
+                  child: ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.green),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20),
+                              side: BorderSide(color: Colors.green))),
+                      padding: MaterialStateProperty.all<EdgeInsets>(
+                          EdgeInsets.symmetric(vertical: 20)),
+                    ),
+                    onPressed: () {
+                      // login();
+                    },
+                    child: Text(
+                      "Update",
+                      style: TextStyle(fontSize: 20),
+                    ),
                   ),
                 ),
-              ),
-              Padding(
-                 padding:
-                    const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
-                child: Container(
-                  width:MediaQuery.of(context).size.height*0.6,
-                  height:50,
-                  child: ElevatedButton(onPressed:() {
-                    
-                  }, 
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Color(0xFF296157),
-                    foregroundColor: Color(0XFFFFFFFF)
-                  ),
-                  child: Text("Upload Image",style: TextStyle(fontSize:20),)),
+                SizedBox(
+                  height: 20,
                 ),
-              ),
-               Padding(
-                 padding:
-                    const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
-                 child: Container(
-                  width:MediaQuery.of(context).size.height*0.6,
-                  height:50,
-                  child: ElevatedButton(onPressed:() {
-                    
-                  }, 
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Color(0xFF77ABA2),
-                    foregroundColor: Color(0XFFFFFFFF)
+                Container(
+                  width: 300,
+                  child: ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.green),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20),
+                              side: BorderSide(color: Colors.green))),
+                      padding: MaterialStateProperty.all<EdgeInsets>(
+                          EdgeInsets.symmetric(vertical: 20)),
+                    ),
+                    onPressed: () {
+                      // login();
+                    },
+                    child: Text(
+                      "Cancel",
+                      style: TextStyle(fontSize: 20),
+                    ),
                   ),
-                  child: Text("Cancel",style: TextStyle(fontSize:20),)),
-                             ),
-               )
-             
-            ],
-          ),
-        ),
-      ),
-    );
+                ),
+              ],
+            ),
+          ));
   }
 }
