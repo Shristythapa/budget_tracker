06e7d745ba0a71fa18dbe71dfc7d5b336b6b0e93
sumansahs <sumansah900@gmail.com>
2023-02-16 09:09:46 +0545

    upadted userprofile

--- lib/theme Swatcher/userProfile.dart
+++ lib/theme Swatcher/userProfile.dart
@@ -1,145 +1,147 @@
 // import 'package:animated_theme_switcher/animated_theme_switcher.dart';
-// import 'package:budget_tracer_practice/theme%20Swatcher/theme_service.dart';
+import 'package:animated_theme_switcher/animated_theme_switcher.dart';
+import 'package:budget_tracer_practice/theme%20Swatcher/theme_service.dart';
+import 'package:day_night_switcher/day_night_switcher.dart';
 // import 'package:day_night_switcher/day_night_switcher.dart';
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-// import 'alert_dialogbox.dart';
+import 'alert_dialogbox.dart';
 
 
 
-// class userprofile extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return ThemeSwitchingArea(
-//       child: Scaffold(
-//         appBar: AppBar(title: const Text('Flutter Dark Mode'),
-//             actions: [
-//               ThemeSwitcher(
-//                 builder: (context) {
-//                   bool isDarkMode =
-//                       ThemeModelInheritedNotifier.of(context).theme.brightness ==
-//                           Brightness.light;
-//                   String themeName = isDarkMode ? 'dark' : 'light';
-//                   return DayNightSwitcherIcon(
-//                     isDarkModeEnabled: isDarkMode,
-//                     onStateChanged: (bool darkMode) async {
-//                       var service = await ThemeService.instance
-//                         ..save(darkMode ? 'light' : 'dark');
-//                       var theme = service.getByName(themeName);
-//                       ThemeSwitcher.of(context)
-//                           .changeTheme(theme: theme, isReversed: darkMode);
-//                     },
-//                   );
-//                 },
-//               ),
-//             ] ),
-//         body: Stack(
-//           alignment: Alignment.center,
-//           children: [
-//             Column(
-//               crossAxisAlignment: CrossAxisAlignment.center,
-//               children: [
-//                 const Padding(
-//                   padding: EdgeInsets.all(20),
-//                   child: Text(
-//                     "My Profile",
-//                     style: TextStyle(
-//                       fontSize: 30,
-//                       letterSpacing: 1.5,
-//                       color: Colors.white,
-//                       fontWeight: FontWeight.w600,
-//                     ),
-//                   ),
-//                 ),
-//                 Container(
-//                   width: MediaQuery.of(context).size.width / 100 * 80,
-//                   height: MediaQuery.of(context).size.height / 100 * 50,
-//                   padding: EdgeInsets.all(20),
-//                   margin: EdgeInsets.all(30),
-//                   decoration: BoxDecoration(boxShadow: [
-//                     BoxShadow(
-//                       color: Colors.lightGreen,
-//                       // spreadRadius: .5,
-//                       // blurRadius: 2,
-//                     ),
-//                   ], borderRadius: BorderRadius.circular(30)),
-//                   child: Column(
-//                     children: [
-//                       Container(
-//                         padding: EdgeInsets.all(10.0),
-//                         width: MediaQuery.of(context).size.width / 3,
-//                         height: MediaQuery.of(context).size.width / 3,
-//                         decoration: BoxDecoration(
-//                           border: Border.all(color: Colors.white, width: 5),
-//                           shape: BoxShape.circle,
-//                           color: Colors.white,
-//                           image: DecorationImage(
-//                             fit: BoxFit.cover,
-//                             image: AssetImage("assets/images/profile.jpg"),
-//                           ),
-//                         ),
-//                       ),
-//                       SizedBox(
-//                         height: 20,
-//                       ),
-//                       Text("Rebecaa Anderson"),
-//                       SizedBox(
-//                         height: 10,
-//                       ),
-//                       Text("rebecaa@Anderson"),
-//                       SizedBox(
-//                         height: 30,
-//                       ),
-//                     ],
-//                   ),
-//                 ),
-//                 Row(
-//                   // crossAxisAlignment: CrossAxisAlignment.,
-//                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//                   children: [
-//                     Container(
-//                         width: MediaQuery.of(context).size.width / 60 * 25,
-//                         height: MediaQuery.of(context).size.height / 100 * 8,
-//                         padding: EdgeInsets.all(10),
-//                         decoration: BoxDecoration(
-//                             color: Colors.lightBlueAccent,
-//                             borderRadius: BorderRadius.circular(30)),
-//                         margin: EdgeInsets.only(right: 40),
-//                         child: TextButton(
-//                             onPressed: () {},
-//                             child: Text("Edit Account",
-//                                 style: TextStyle(
-//                                     color: Color(0xFF296157),
-//                                     fontSize: 17,
-//                                     fontWeight: FontWeight.bold)))),
-//                     Container(
-//                         width: MediaQuery.of(context).size.width / 110 * 25,
-//                         height: MediaQuery.of(context).size.height / 100 * 8,
-//                         padding: EdgeInsets.all(3),
-//                         decoration: BoxDecoration(
-//                             color: Colors.lightBlueAccent,
-//                             borderRadius: BorderRadius.circular(30)),
-//                         child: TextButton(
-//                             onPressed: () async{
-//                               final action = await AlertDialogs.yesCancelDialog(context, 'Logout', 'Are you sure want to logout?');
-//                               if (action == DialogsAction.yes) {
+class userprofile extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ThemeSwitchingArea(
+      child: Scaffold(
+        appBar: AppBar(title: const Text('Flutter Dark Mode'),
+            actions: [
+              ThemeSwitcher(
+                builder: (context) {
+                  bool isDarkMode =
+                      ThemeModelInheritedNotifier.of(context).theme.brightness ==
+                          Brightness.light;
+                  String themeName = isDarkMode ? 'dark' : 'light';
+                  return DayNightSwitcherIcon(
+                    isDarkModeEnabled: isDarkMode,
+                    onStateChanged: (bool darkMode) async {
+                      var service = await ThemeService.instance
+                        ..save(darkMode ? 'light' : 'dark');
+                      var theme = service.getByName(themeName);
+                      ThemeSwitcher.of(context)
+                          .changeTheme(theme: theme, isReversed: darkMode);
+                    },
+                  );
+                },
+              ),
+            ] ),
+        body: Stack(
+          alignment: Alignment.center,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Padding(
+                  padding: EdgeInsets.all(20),
+                  child: Text(
+                    "My Profile",
+                    style: TextStyle(
+                      fontSize: 30,
+                      letterSpacing: 1.5,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width / 100 * 80,
+                  height: MediaQuery.of(context).size.height / 100 * 50,
+                  padding: EdgeInsets.all(20),
+                  margin: EdgeInsets.all(30),
+                  decoration: BoxDecoration(boxShadow: [
+                    BoxShadow(
+                      color: Colors.lightGreen,
+                      // spreadRadius: .5,
+                      // blurRadius: 2,
+                    ),
+                  ], borderRadius: BorderRadius.circular(30)),
+                  child: Column(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.all(10.0),
+                        width: MediaQuery.of(context).size.width / 3,
+                        height: MediaQuery.of(context).size.width / 3,
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.white, width: 5),
+                          shape: BoxShape.circle,
+                          color: Colors.white,
+                          image: DecorationImage(
+                            fit: BoxFit.cover,
+                            image: AssetImage("assets/images/profile.jpg"),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Text("Rebecaa Anderson"),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text("rebecaa@Anderson"),
+                      SizedBox(
+                        height: 30,
+                      ),
+                    ],
+                  ),
+                ),
+                Row(
+                  // crossAxisAlignment: CrossAxisAlignment.,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                        width: MediaQuery.of(context).size.width / 60 * 25,
+                        height: MediaQuery.of(context).size.height / 100 * 8,
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                            color: Colors.lightBlueAccent,
+                            borderRadius: BorderRadius.circular(30)),
+                        margin: EdgeInsets.only(right: 40),
+                        child: TextButton(
+                            onPressed: () {},
+                            child: Text("Edit Account",
+                                style: TextStyle(
+                                    color: Color(0xFF296157),
+                                    fontSize: 17,
+                                    fontWeight: FontWeight.bold)))),
+                    Container(
+                        width: MediaQuery.of(context).size.width / 110 * 25,
+                        height: MediaQuery.of(context).size.height / 100 * 8,
+                        padding: EdgeInsets.all(3),
+                        decoration: BoxDecoration(
+                            color: Colors.lightBlueAccent,
+                            borderRadius: BorderRadius.circular(30)),
+                        child: TextButton(
+                            onPressed: () async{
+                              final action = await AlertDialogs.yesCancelDialog(context, 'Logout', 'Are you sure want to logout?');
+                              if (action == DialogsAction.yes) {
 
-//                               }
-//                             },
-//                             child: Text(
-//                               "Logout",
-//                               style: TextStyle(
-//                                   color: Color(0xFF296157),
-//                                   fontSize: 17,
-//                                   fontWeight: FontWeight.bold),
-//                             ))),
-//                   ],
-//                 )
-//               ],
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
+                              }
+                            },
+                            child: Text(
+                              "Logout",
+                              style: TextStyle(
+                                  color: Color(0xFF296157),
+                                  fontSize: 17,
+                                  fontWeight: FontWeight.bold),
+                            ))),
+                  ],
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
